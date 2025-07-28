8a94f6d7977c8c7c3c4879088b87202861803f6f
DanielaVillalta <20240286@ricaldone.edu.sv>
2025-07-28 10:46:35 -0600

    Configuracion

---
+++ src/main/java/DanielaVillalta_20240286/DanielaVillalta_20240286/DanielaVillalta20240286Application.java
@@ -0,0 +1,13 @@
+package DanielaVillalta_20240286.DanielaVillalta_20240286;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielaVillalta20240286Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielaVillalta20240286Application.class, args);
+	}
+
+}
---
+++ src/test/java/DanielaVillalta_20240286/DanielaVillalta_20240286/DanielaVillalta20240286ApplicationTests.java
@@ -0,0 +1,13 @@
+package DanielaVillalta_20240286.DanielaVillalta_20240286;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielaVillalta20240286ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
